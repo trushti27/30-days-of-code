33ee37ca56b31ea3376e04a103e5b5885e46367b
trushti27 <trushtirupapara@gmail.com>
2025-08-05 18:50:50 +0530

    day23

---
+++ day23.cpp
@@ -0,0 +1,26 @@
+//// LeetCode Problem: Valid Parentheses
+// Problem Link: https://leetcode.com/problems/valid-parentheses/
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i=0; i<s.length(); i++)
+        {
+            if(s[i]=='{' || s[i]=='(' || s[i]=='[')
+                st.push(s[i]);
+            else
+            {
+               if(st.empty())
+                    return false;
+                if((s[i]=='}' && st.top()!='{') ||
+                   (s[i]==')' && st.top()!='(') ||
+                   (s[i]==']' && st.top()!='['))
+                    return false;
+                st.pop();
+            } 
+           
+        }
+        return st.empty();
+    }
+};
